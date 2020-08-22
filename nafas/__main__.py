d7f07e38c014946cf933e8fdfb9ee5990c372a53
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-08-22 20:07:32 +0430

    fix : __main__ file updated

--- nafas/__main__.py
+++ nafas/__main__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """nafas main."""
 
+from .functions import *
+
 if __name__ == "__main__":
-    pass
+    input_data = get_input_standard()
+    filtered_data = input_filter(input_data)
+    program_data = get_program_dict(filtered_data)
+    run(program_data)
