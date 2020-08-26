eb43c4a4036dd942a1f4840c1daf24d17994f628
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-08-26 13:37:06 +0430

    fix : minor edit in main #1

--- nafas/__main__.py
+++ nafas/__main__.py
@@ -10,12 +10,10 @@ if __name__ == "__main__":
     args = sys.argv
     tprint("Nafas")
     tprint("v" + str(NAFAS_VERSION))
-    if len(args) > 1:
-        if args[1].upper() == "TEST":
-            description_print(lambda x : "1")
-    else:
-        description_print()
-    input_data = get_input_standard()
+    description_print()
+    input_data = {"program":1,"level":1}
+    if len(args) < 1:
+        input_data = get_input_standard()
     filtered_data = input_filter(input_data)
     program_data = get_program_dict(filtered_data)
     run(program_data)
