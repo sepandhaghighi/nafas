50b8e61d2d8714f2066d69fbd686e521e15ac775
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-01-31 09:52:08 +0330

    fix : menu optimized #30

--- nafas/__main__.py
+++ nafas/__main__.py
@@ -12,8 +12,13 @@ if __name__ == "__main__":
     tprint("v" + str(NAFAS_VERSION))
     description_print()
     if len(args) < 2:
-        input_data = get_input_standard()
-        filtered_data = input_filter(input_data)
-        program_name, level, program_data = get_program_data(filtered_data)
-        program_description_print(program_name, level, program_data)
-        run(program_data)
+        EXIT_FLAG = False
+        while not EXIT_FLAG:
+            input_data = get_input_standard()
+            filtered_data = input_filter(input_data)
+            program_name, level, program_data = get_program_data(filtered_data)
+            program_description_print(program_name, level, program_data)
+            run(program_data)
+            INPUTINDEX = str(input("Press [R] to restart or any other key to exit."))
+            if INPUTINDEX.upper() != "R":
+                EXIT_FLAG = True
